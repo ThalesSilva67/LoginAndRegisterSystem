463669a9e20643f31546a6f6fa2b85889ff78db8
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-08 16:55:35 -0300

    Initial commit

---
+++ src/main/java/com/account/system/entity/User.java
@@ -0,0 +1,69 @@
+package com.account.system.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table
+public class User {
+    @Id
+    @GeneratedValue
+    private long id;
+    @NotBlank(message = "O nome é obrigatório")
+    private String name;
+    @NotBlank(message = "O email é obrigatório")
+    @Email
+    @Column(unique = true)
+    private String email;
+    @NotNull
+    @NotBlank
+    @Size(min = 8, max=16)
+    private String password;
+
+    public User(Long id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
