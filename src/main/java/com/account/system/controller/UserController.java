8d6cbf5982f0f8b61bc7dbcac265d910c769671b
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-11 16:52:54 -0300

    feat(UserController): create register post mapping methodAdds a new POST /register endpoint to handle user registration data, implementing validation to ensure data integrity.

---
+++ src/main/java/com/account/system/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.account.system.controller;
+
+import com.account.system.dto.request.UserRegisterDTO;
+import com.account.system.dto.response.UserResponseDTO;
+import com.account.system.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    private final UserService service;
+
+    public UserController(UserService userService) {
+        this.service = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> register(@RequestBody UserRegisterDTO user) {
+        UserResponseDTO response = service.register(user);
+
+        return ResponseEntity.ok(response);
+    }
+}
