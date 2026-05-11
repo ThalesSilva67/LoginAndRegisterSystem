f087c85b60f2fc66176e6e0f3e021dbdf339f370
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-11 16:42:58 -0300

    feat(UserService): add register method and repository constructor injection

---
+++ src/main/java/com/account/system/service/UserService.java
@@ -0,0 +1,32 @@
+package com.account.system.service;
+
+import com.account.system.dto.request.UserRegisterDTO;
+import com.account.system.dto.response.UserResponseDTO;
+import com.account.system.entity.User;
+import com.account.system.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private final UserRepository repository;
+
+    public UserService(UserRepository userRepository) {
+        repository = userRepository;
+    }
+
+    public UserResponseDTO register(UserRegisterDTO userRegisterDTO) {
+        boolean present = repository.findByEmail(userRegisterDTO.email()).isPresent();
+
+        if(present) throw new RuntimeException("Email already exists");
+
+        User user = new User();
+        user.setName(userRegisterDTO.name());
+        user.setEmail(userRegisterDTO.email());
+        user.setPassword(userRegisterDTO.password());
+
+        User savedUser = repository.save(user);
+
+        return new UserResponseDTO(savedUser.getId(), savedUser.getName(), savedUser.getEmail());
+    }
+
+}
