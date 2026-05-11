40e148f5c9575570a48f3309f6cdc319387f2bfc
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-11 16:40:29 -0300

    feat(user): add request and response DTOs for http requests

---
+++ src/main/java/com/account/system/dto/request/UserRegisterDTO.java
@@ -0,0 +1,4 @@
+package com.account.system.dto.request;
+
+public record UserRegisterDTO(String name, String email, String password) {
+}
---
+++ src/main/java/com/account/system/dto/response/UserResponseDTO.java
@@ -0,0 +1,4 @@
+package com.account.system.dto.response;
+
+public record UserResponseDTO(Long id, String name, String email) {
+}
