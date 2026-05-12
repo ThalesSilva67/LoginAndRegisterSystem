dc68a92975791633ddf3ab74be2209ef9c4a4b36
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-12 16:00:22 -0300

    feat(DTO): create login DTO to response and request

---
+++ src/main/java/com/account/system/dto/request/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.account.system.dto.request;
+
+public record LoginRequestDTO(String email, String password) {
+}
---
+++ src/main/java/com/account/system/dto/response/LoginResponseDTO.java
@@ -0,0 +1,4 @@
+package com.account.system.dto.response;
+
+public record LoginResponseDTO(String message) {
+}
