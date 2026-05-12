0ae7b02d674b373febbe3cae8e3efd51e81e231d
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-12 15:34:05 -0300

    feat(security): add password encoder bean and enable h2-console

--- src/main/java/com/account/system/configuration/SecurityConfigurations.java
+++ src/main/java/com/account/system/configuration/SecurityConfigurations.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,12 +19,18 @@ public class SecurityConfigurations {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers(HttpMethod.POST, "/users/register").permitAll();
                     req.requestMatchers("/h2-console/**").permitAll();
                     req.anyRequest().authenticated();
                 })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
