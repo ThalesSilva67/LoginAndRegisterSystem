5af6b26a11635e91bfb06dde0859961f2853d2b0
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-12 15:05:59 -0300

    feat(security): configure web security and authorization filters

--- src/main/java/com/account/system/configuration/SecurityConfigurations.java
+++ src/main/java/com/account/system/configuration/SecurityConfigurations.java
@@ -1,7 +1,26 @@
 package com.account.system.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfiguration {
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers(HttpMethod.POST, "/users/register").permitAll();
+                    req.anyRequest().authenticated();
+                })
+                .build();
+    }
+
 }
