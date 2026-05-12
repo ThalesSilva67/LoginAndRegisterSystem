7bf880cb6dbfa70462b4cd8b09f4955d63567898
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-12 15:35:01 -0300

    feat(service): inject passwordEncoder supporting hash in passwords

--- src/main/java/com/account/system/service/UserService.java
+++ src/main/java/com/account/system/service/UserService.java
@@ -1,17 +1,21 @@
 package com.account.system.service;
 
+import com.account.system.configuration.SecurityConfigurations;
 import com.account.system.dto.request.UserRegisterDTO;
 import com.account.system.dto.response.UserResponseDTO;
 import com.account.system.entity.Users;
 import com.account.system.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository repository;
+    private final PasswordEncoder encoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, SecurityConfigurations security, PasswordEncoder encoder) {
         repository = userRepository;
+        this.encoder = encoder;
     }
 
     public UserResponseDTO register(UserRegisterDTO userRegisterDTO) {
@@ -22,7 +26,7 @@ public class UserService {
         Users user = new Users();
         user.setName(userRegisterDTO.name());
         user.setEmail(userRegisterDTO.email());
-        user.setPassword(userRegisterDTO.password());
+        user.setPassword(encoder.encode(userRegisterDTO.password()));
 
         Users savedUser = repository.save(user);
 
