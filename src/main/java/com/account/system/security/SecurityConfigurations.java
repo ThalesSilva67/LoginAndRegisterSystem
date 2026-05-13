f698e267c7bc58fe8a9f460352c790af2069d83e
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-13 17:02:52 -0300

    feat(security): add jwtFilter and authFilter security.

--- src/main/java/com/account/system/security/SecurityConfigurations.java
+++ src/main/java/com/account/system/security/SecurityConfigurations.java
@@ -1,5 +1,7 @@
-package com.account.system.configuration;
+package com.account.system.security;
 
+import com.account.system.security.jwt.JwtAuthenticationEntryPoint;
+import com.account.system.security.jwt.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -10,10 +12,18 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
+    private final JwtAuthenticationFilter authenticationFilter;
+    private final JwtAuthenticationEntryPoint authEntryPoint;
+
+    public SecurityConfigurations(JwtAuthenticationFilter authenticationFilter,  JwtAuthenticationEntryPoint authEntryPoint) {
+        this.authenticationFilter = authenticationFilter;
+        this.authEntryPoint = authEntryPoint;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -21,11 +31,13 @@ public class SecurityConfigurations {
                 .csrf(AbstractHttpConfigurer::disable)
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .authorizeHttpRequests(req -> {
-                    req.requestMatchers(HttpMethod.POST, "/users/register").permitAll();
-                    req.requestMatchers(HttpMethod.POST, "/users/login").permitAll();
+                    req.requestMatchers(HttpMethod.POST, "/account/register").permitAll();
+                    req.requestMatchers(HttpMethod.POST, "/account/login").permitAll();
                     req.requestMatchers("/h2-console/**").permitAll();
                     req.anyRequest().authenticated();
                 })
+                .exceptionHandling(ex -> ex.authenticationEntryPoint(authEntryPoint))
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
