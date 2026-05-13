c999a27c58dbae8457e73a53f760944279ff4515
Thales Oliveira S. Costa <thalesslva67@gmail.com>
2026-05-13 17:01:51 -0300

    feat(account controller): rename file usersController to accountController and add generate token in routes auth

--- src/main/java/com/account/system/controller/AccountController.java
+++ src/main/java/com/account/system/controller/AccountController.java
@@ -4,7 +4,9 @@ import com.account.system.dto.request.LoginRequestDTO;
 import com.account.system.dto.request.UserRegisterDTO;
 import com.account.system.dto.response.LoginResponseDTO;
 import com.account.system.dto.response.UserResponseDTO;
-import com.account.system.service.UserService;
+import com.account.system.entity.Users;
+import com.account.system.security.jwt.JwtProvider;
+import com.account.system.service.AccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,12 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/users")
-public class UserController {
-    private final UserService service;
+@RequestMapping("/account")
+public class AccountController {
+    private final AccountService service;
+    private final JwtProvider jwtProvider;
 
-    public UserController(UserService userService) {
-        this.service = userService;
+    public AccountController(AccountService accountService, JwtProvider jwtProvider) {
+        this.service = accountService;
+        this.jwtProvider = jwtProvider;
     }
 
     @PostMapping("/register")
@@ -29,7 +33,11 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO user) {
-        LoginResponseDTO response = service.login(user.email(), user.password());
+        Users login = service.login(user.email(), user.password());
+
+        String token = jwtProvider.generateToken(login);
+
+        LoginResponseDTO response = new LoginResponseDTO(token);
 
         return ResponseEntity.ok(response);
     }
